435b4ba9117c347cf658c3a86e27427a6992688e
cullinans1 <sammy.cullinan@gmail.com>
2021-04-30 20:06:55 +0000

    update

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1 style=\"text-align: center\">Welcome to CloudForce application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1 style=\"text-align: center\">Welcome to CloudForce Application</h1>"));
         return done();
       });
 
